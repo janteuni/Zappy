d8b77534ff727622137f3c427b23b5ec0ecceff5
janteuni <janteuni@student.42.fr>
2014-06-10 16:21:22 +0200

    check leaks

--- dir_serveur/src/ft_graphic_function.c
+++ dir_serveur/src/ft_graphic_function.c
@@ -1,33 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_treat_eat.c                                     :+:      :+:    :+:   */
+/*   ft_graphic_function.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/06/09 18:36:41 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/10 15:44:33 by janteuni         ###   ########.fr       */
+/*   Created: 2014/06/10 15:37:24 by janteuni          #+#    #+#             */
+/*   Updated: 2014/06/10 15:44:24 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serveur.h"
 
-void				ft_treat_eat(t_env *env)
+void				ft_graphic_function(t_env *env, int cs, char *rcv)
 {
-	int				i;
-
-	i = 0;
-	printf("I EAT\n");
-	while (i < env->max_fd)
-	{
-		if (env->fd_socket[i].type == CLIENT)
-		{
-			env->fd_socket[i].inventory[FOOD] -= 1;
-			if (env->fd_socket[i].inventory[FOOD] <= 0)
-			{
-				ft_reply_in_buff(env, i, "mort");
-			}
-		}
-		i++;
-	}
+	printf("graphic cmd\n");
+	(void)env;
+	(void)cs;
+	(void)rcv;
 }
