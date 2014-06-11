8b7d06179eb2b9b93b540e82d7a33570cc598c93
janteuni <janteuni@student.42.fr>
2014-06-11 13:05:20 +0200

    no leaks

--- dir_serveur/src/ft_treat_inventory.c
+++ dir_serveur/src/ft_treat_inventory.c
@@ -6,20 +6,12 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/07 11:36:48 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/11 11:57:22 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/11 13:00:34 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "serveur.h"
-
-static void			st_send(char *ret, t_env *env, int cs)
-{
-	ret[ft_strlen(ret) - 1] = '\0';
-	ret[ft_strlen(ret) - 1] = '}';
-	ft_reply_in_buff(env, cs, ret);
-	ft_memdel((void **)&ret);
-}
 
 void				ft_treat_inventory(t_env *env, int cs, char *rcv)
 {
@@ -44,7 +36,11 @@ void				ft_treat_inventory(t_env *env, int cs, char *rcv)
 		ft_memdel((void **)&ret);
 		ft_memdel((void **)&itoa);
 		ret = ft_strjoin(tmp, ", ");
+		ft_memdel((void **)&tmp);
 		i++;
 	}
-	st_send(ret, env, cs);
+	ret[ft_strlen(ret) - 1] = '\0';
+	ret[ft_strlen(ret) - 1] = '}';
+	ft_reply_in_buff(env, cs, ret);
+	ft_memdel((void **)&ret);
 }
