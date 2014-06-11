e2a69af71df13b9512acb721fffc0860c3d444fc
fbeck <fbeck@student.42.fr>
2014-06-11 12:54:46 +0200

    adidng seperate functions to send moves

--- dir_client/src/get_env.c
+++ dir_client/src/get_env.c
@@ -6,20 +6,20 @@
 /*   By: janteuni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/06 16:40:35 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/11 11:53:33 by fbeck            ###   ########.fr       */
+/*   Updated: 2014/06/11 12:50:41 by fbeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "client.h"
-
+/*
 int				init_cmd_tab(t_env *env)
 {
 	if (!(env->cmds = (int *)malloc(NB_CMDS * sizeof(int))))
 		return (ERR);
 	ft_bzero(env->cmds, (NB_CMDS * sizeof(int)));
 	return (OK);
-}
+}*/
 
 int				init_responces(t_env *env)
 {
@@ -36,7 +36,7 @@ t_env			*get_env(void)
 	if (!env)
 	{
 		if (!(env = (t_env *)malloc(sizeof(t_env)))
-				|| (init_cmd_tab(env) == ERR)
+			/*	|| (init_cmd_tab(env) == ERR)*/
 				|| (init_responces(env) == ERR))
 		{
 			error("Malloc Failed env");
