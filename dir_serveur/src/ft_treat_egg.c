922f71d11d6b49a5f69bed07b6e6e76882879458
janteuni <janteuni@student.42.fr>
2014-06-13 12:24:12 +0200

    fork ok

--- dir_serveur/src/ft_treat_egg.c
+++ dir_serveur/src/ft_treat_egg.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/11 15:40:17 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/13 11:54:39 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/13 12:23:48 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 
 void				ft_treat_egg(t_env *env, int cs, char *rcv)
 {
+	int				i;
+
+	i = 0;
 	(void)rcv;
 	printf("EGG IS OOOOOOKKKKK\n");
+	while (i < env->max_team)
+	{
+		if (ft_strcmp(env->teams[i].name, env->fd_socket[cs].my_team) == 0)
+		{
+			env->teams[i].max_player++;
+			break ;
+		}
+		i++;
+	}
 	ft_graphic_reply(env, cs, ft_graphic_eht);
 }
--- dir_serveur/src/ft_treat_fork.c
+++ dir_serveur/src/ft_treat_fork.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/09 12:10:25 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/13 11:52:14 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/13 12:23:57 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,7 @@
 
 void				ft_treat_fork(t_env *env, int cs, char *rcv)
 {
-	int				i;
-
-	i = 0;
 	(void)rcv;
-	while (i < env->max_team)
-	{
-		if (ft_strcmp(env->teams[i].name, env->fd_socket[cs].my_team) == 0)
-		{
-			env->teams[i].max_player++;
-			break ;
-		}
-		i++;
-	}
 	ft_reply_in_buff(env, cs, "ok");
 	ft_graphic_reply(env, cs, ft_graphic_enw);
 	ft_action_add(cs, 600, ft_treat_egg, "egg");
