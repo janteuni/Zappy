c6a396fabf0057fede8b016b771058d11b49a0fc
janteuni <janteuni@student.42.fr>
2014-06-18 14:42:24 +0200

    messages orientation

--- dir_serveur/src/ft_treat_left.c
+++ dir_serveur/src/ft_treat_left.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/06 19:52:26 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/17 17:30:37 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/18 14:39:33 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void				ft_treat_left(t_env *env, int cs, char *rcv)
 	else if (OR(cs) == E)
 		OR(cs) = N;
 	ft_reply_in_buff(env, cs, "ok");
+	printf("ORIENTATION : %d\n", OR(cs));
 	ft_graphic_reply(env, cs, ft_graphic_ppo);
 }
--- dir_serveur/src/ft_treat_right.c
+++ dir_serveur/src/ft_treat_right.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/09 15:36:20 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/13 11:13:18 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/18 14:39:31 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void				ft_treat_right(t_env *env, int cs, char *rcv)
 	else if (OR(cs) == O)
 		OR(cs) = N;
 	ft_reply_in_buff(env, cs, "ok");
+	printf("ORIENTATION : %d\n", OR(cs));
 	ft_graphic_reply(env, cs, ft_graphic_ppo);
 }
