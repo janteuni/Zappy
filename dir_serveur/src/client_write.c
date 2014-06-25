9f0d31665cef3546021948f45f78a11c03b7d045
janteuni <janteuni@student.42.fr>
2014-06-25 17:05:34 +0200

    server: quit: just 2 send

--- dir_serveur/src/client_write.c
+++ dir_serveur/src/client_write.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/20 18:15:48 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/23 12:33:00 by mpillet          ###   ########.fr       */
+/*   Updated: 2014/06/25 16:58:28 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,13 @@ static void		st_loop(char **join, char **tmp, t_list *list)
 
 static void		st_clean(t_env *env, char *tmp, int cs)
 {
+	if (cs == env->graphic && ft_strstr(tmp, "seg ") != NULL)
+		env->end = YES;
 	ft_lstdel(&(env->fd_socket[cs].line), ft_del);
-	ft_memdel((void **)&tmp);
 	ft_bzero(env->fd_socket[cs].buf_write, BUF_SIZE);
+	if (env->fd_socket[cs].type == CLIENT && ft_strstr(tmp, "mort\n") != NULL)
+		clean_fd(&env->fd_socket[cs]);
+	ft_memdel((void **)&tmp);
 }
 
 void			client_write(t_env *env, int cs)
--- dir_serveur/src/signal.c
+++ dir_serveur/src/signal.c
@@ -6,7 +6,7 @@
 /*   By: janteuni <janteuni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/17 16:00:48 by janteuni          #+#    #+#             */
-/*   Updated: 2014/06/18 16:15:21 by janteuni         ###   ########.fr       */
+/*   Updated: 2014/06/25 17:04:44 by janteuni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,28 @@
 #include <sys/socket.h>
 #include "serveur.h"
 
+static void			st_special_quit(t_env *env)
+{
+	int				i;
+
+	i = 0;
+	while (i < env->max_fd)
+	{
+		if (env->fd_socket[i].type == CLIENT && i != env->graphic)
+		{
+			printf("mort de %d LEVEL %d\n", i, env->fd_socket[i].level);
+			send(i, "mort\n", 5, 0);
+		}
+		i++;
+	}
+}
+
 void				ft_ctr_c(int i)
 {
 	t_env		*env;
 
 	(void)i;
 	env = get_env();
-	ft_treat_dead_all(env);
-	if (env->graphic != -1)
-		send(env->graphic, "seg CTR-C\n", 10, 0);
+	st_special_quit(env);
 	ft_exit("ctr c signal");
 }
